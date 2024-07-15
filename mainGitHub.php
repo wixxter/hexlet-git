a947c0257cd89de30fbcb977d407a7a1c557c2f5
wixxter <wixxters@gmail.com>
2024-07-15 21:40:47 +0500

    add new content

--- mainGitHub.php
+++ mainGitHub.php
@@ -1,3 +1,26 @@
 <?php
 
-print_r(4*5);
+$data = [
+    'user' => 'ubuntu',
+    'hosts' => [
+        ['name' => 'web1'],
+        ['name' => 'web2', null => 3, 'active' => false]
+    ]
+];
+
+function getIn($data, $items)
+{
+    $temp = $data;
+    foreach ($items as $item) {
+        print_r($temp);
+        if (!is_array($temp) || !array_key_exists($item, $temp)) {
+            return null;
+        }
+
+        $temp = $temp[$item];
+    }
+
+    return $temp;
+}
+
+getIn($data, ['hosts', 1, 'active']);
